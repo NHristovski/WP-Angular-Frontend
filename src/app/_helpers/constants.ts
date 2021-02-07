4ea054aaf30b19c0ca44ac923b9679e72d088312
Nikola Hristovski <nikolahristovski1122@gmail.com>
2021-02-07 15:47:58 +0100

    Changed the base api url

--- src/app/_helpers/constants.ts
+++ src/app/_helpers/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  public static baseApiUrl = 'http://35.242.235.152:8762';
+  public static baseApiUrl = 'http://35.228.10.240:8762';
   // public static baseApiUrl = 'http://localhost:8762';
   // 'http://localhost:8762'; // 'http://api-gateway:8762';
   // public static baseApiUrl = 'https://wp-api-gateway.herokuapp.com';
